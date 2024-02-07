b06ecbf8505b1279b4be575beedd84895d5fbee7
benzakielarz <bjzakielarz@loyola.edu>
2024-02-07 13:28:58 -0500

    withdraw and exit

--- Lab3.java
+++ Lab3.java
@@ -19,14 +19,28 @@ class Lab3 {
         System.out.println("Welcome to the bank, please enter your name: ");
         String name = scanner.nextLine();
 
-        System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
-        String choice = scanner.nextLine();
+        String choice = "";
 
-        if(choice.equals("D")){
-            System.out.println("How much would you like to deposit? ");
-            double deposit = Double.parseDouble(scanner.nextLine());
-            balance += deposit;
+        while(!choice.equals("e")) {
+
+            System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
+            choice = scanner.nextLine();
+
+            if (choice.equals("D")) {
+                System.out.println("How much would you like to deposit? ");
+                double deposit = Double.parseDouble(scanner.nextLine());
+                balance += deposit;
+            } else if (choice.equals("W")) {
+                System.out.println("How much would you like to withdraw? ");
+                double withdraw = Double.parseDouble(scanner.nextLine());
+                if (withdraw > balance) {
+                    while (withdraw > balance) {
+                        System.out.println("Please enter a valid withdraw amount. (Must be less than or equal to your current balance: " + balance + ") ");
+                        withdraw = Double.parseDouble(scanner.nextLine());
+                    }
+                }
+                balance -= withdraw;
+            }
         }
-
     }
 }
