45d399d8ee80e9d336add031c05da7add5f5d8da
04bmoore <bsmoore@loyola.edu>
2024-02-07 13:43:19 -0500

    format

--- Lab3.java
+++ Lab3.java
@@ -9,40 +9,40 @@
 //            Reminder: you should never take code from the Internet or another person
 
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 class Lab3 {
     public static void main(String[] args) {
 
         double balance = 212.90;
 
+        DecimalFormat df = new DecimalFormat("$0.00");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the bank, please enter your name: ");
         String name = scanner.nextLine();
 
         String choice = "";
 
-        while(!choice.equals("e")) {
-        System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
-        String choice = scanner.nextLine();
+        while (!choice.equals("e")) {
+            System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
+            choice = scanner.nextLine();
 
-        if(choice.equals("D")){
-            System.out.println("How much would you like to deposit? ");
-            double deposit = Double.parseDouble(scanner.nextLine());
-            balance += deposit;
-        }else if (choice.equals("W")) {
-            System.out.println("How much would you like to withdraw? ");
-            double withdraw = Double.parseDouble(scanner.nextLine());
-            if (withdraw > balance) {
-                while (withdraw > balance) {
-                    System.out.println("Please enter a valid withdraw amount. (Must be less than or equal to your current balance: " + balance + ") ");
-                    withdraw = Double.parseDouble(scanner.nextLine());
+            if (choice.equals("D")) {
+                System.out.println("How much would you like to deposit? ");
+                double deposit = Double.parseDouble(scanner.nextLine());
+                balance += deposit;
+            } else if (choice.equals("W")) {
+                System.out.println("How much would you like to withdraw? ");
+                double withdraw = Double.parseDouble(scanner.nextLine());
+                if (withdraw > balance) {
+                    while (withdraw > balance) {
+                        System.out.println("Please enter a valid withdraw amount. (Must be less than or equal to your current balance: " + balance + ") ");
+                        withdraw = Double.parseDouble(scanner.nextLine());
+                    }
                 }
+                balance -= withdraw;
+            } else if (choice.equals("c")) {
+                System.out.println(name + " has a current balance of " + df.format(balance));
             }
-            balance -= withdraw;
-        }
-        else if(choice.equals("c")){
-            System.out.println(name + "has a current balance of $" + df.format(balance));
         }
     }
 }
-
