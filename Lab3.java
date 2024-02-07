5c573334ae053c98d990b8a1af8b03d052fbadcf
benzakielarz <bjzakielarz@loyola.edu>
2024-02-07 13:14:26 -0500

    Algorithm and initial setup stuff

--- Lab3.java
+++ Lab3.java
@@ -6,14 +6,18 @@
 // Data In:
 // Data Out:
 // Credits: [Is your code based on an example in the book, in class, or something else?
-//            Reminder: you should never take code from the Internet or another person
+// Reminder: you should never take code from the Internet or another person
 
 import java.util.Scanner;
 
 class Lab3 {
     public static void main(String[] args) {
-       System.out.println("Welcome to Lab 3");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the bank, please enter your name: ");
+        String name = scanner.nextLine();
 
+        System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
+        String choice = scanner.nextLine();
 
     }
 }
