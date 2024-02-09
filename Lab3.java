0ac082995acbcda791affdf3505c21a43dbc3b64
04bmoore <bsmoore@loyola.edu>
2024-02-09 16:43:15 -0500

    Final

--- Lab3.java
+++ Lab3.java
@@ -8,8 +8,10 @@
 // Credits: [Is your code based on an example in the book, in class, or something else?
 //            Reminder: you should never take code from the Internet or another person
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.text.DecimalFormat;
+
 class Lab3 {
     public static void main(String[] args) {
 
@@ -21,28 +23,86 @@ class Lab3 {
         String name = scanner.nextLine();
 
         String choice = "";
-
+        Double allDeposits = 0.0;
+        Double allWithdraws = 0.0;
         while (!choice.equals("e")) {
             System.out.println("Welcome, " + name + ". Would you like to deposit (enter 'd'), withdraw (enter 'w'), check your balance (enter 'c'), or exit (enter 'e')");
-            choice = scanner.nextLine();
-
-            if (choice.equals("D")) {
+            choice = scanner.nextLine().toLowerCase();
+            while(!choice.equals("e") && !choice.equals("d") && !choice.equals("w") && !choice.equals("c")){
+                System.out.println("Enter a letter from: d, w, c, e");
+                choice = scanner.nextLine().toLowerCase();
+            } if (choice.equals("d")) {
                 System.out.println("How much would you like to deposit? ");
-                double deposit = Double.parseDouble(scanner.nextLine());
+                Double deposit = null;
+                boolean validDep = false;
+                while (validDep == false) {
+                    while (deposit == null) {
+                        try {
+                            deposit = scanner.nextDouble();
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid input. Please enter a valid number.");
+                            scanner.next();
+                        }
+                    }
+                    if (deposit <= 0){
+                        deposit = null;
+                        System.out.println("Invalid input. Please enter a valid number.");
+                    } else{
+                        validDep = true;
+                    }
+                }
+                allDeposits += deposit;
                 balance += deposit;
-            } else if (choice.equals("W")) {
+
+            } else if (choice.equals("w")) {
                 System.out.println("How much would you like to withdraw? ");
-                double withdraw = Double.parseDouble(scanner.nextLine());
+                Double withdraw = null;
+                boolean validWith = false;
+                while (validWith == false) {
+                    while (withdraw == null) {
+                        try {
+                            withdraw = scanner.nextDouble();
+
+                        } catch (InputMismatchException e) {
+                            System.out.println("Invalid input. Please enter a valid number.");
+                            scanner.next();
+                        }
+                    }
+                    if (withdraw <= 0){
+                        withdraw = null;
+                        System.out.println("Invalid input. Please enter a valid number.");
+                    } else{
+                        validWith = true;
+                    }
+                }
+
                 if (withdraw > balance) {
                     while (withdraw > balance) {
                         System.out.println("Please enter a valid withdraw amount. (Must be less than or equal to your current balance: " + balance + ") ");
                         withdraw = Double.parseDouble(scanner.nextLine());
                     }
                 }
+                allWithdraws += withdraw;
                 balance -= withdraw;
             } else if (choice.equals("c")) {
                 System.out.println(name + " has a current balance of " + df.format(balance));
             }
         }
+        System.out.println("Would you like a receipt printed or emailed? Enter 'print' or 'email' ");
+        String receipt = scanner.nextLine().toLowerCase();
+        while(!receipt.equals("print") && !receipt.equals("email")){
+            System.out.println("Please enter 'print' or 'email'");
+            System.out.println(receipt);
+            receipt = scanner.nextLine().toLowerCase();
+        }
+        System.out.println("You deposited a total of " + df.format(allDeposits));
+        System.out.println("You withdrew a total of " + df.format(allWithdraws));
+        System.out.println("Thank you for banking with us today " + name);
+        if (receipt.equals("print")){
+            System.out.println("Please remember to take your receipt.");
+        } else{
+            System.out.println("Remember to check your email for your receipt.");
+        }
+
     }
 }
